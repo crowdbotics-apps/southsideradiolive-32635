28442dbdc8d96e74e8b38fb72fefdfbba0ce28dc
Crowdbotics <team@crowdbotics.com>
2021-12-29 17:03:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "southsideradiolive_32635",
+  url: "https://southsideradiolive_32635.botics.co",
+  api: "https://southsideradiolive_32635.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
